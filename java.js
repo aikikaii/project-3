2739ab8933f723309ed9166f7209d44d27aeb36f
aikikaii <53414211+aikikaii@users.noreply.github.com>
2019-11-02 19:05:06 -0400

    Update java.js

---
+++ java.js
@@ -0,0 +1,86 @@
+//i am setting focus on first text field
+
+$('#name').focus();
+
+//when user chooses option Other from Job Role section, make text field in HTML and hide it via jquery
+
+$('#other-title').hide();
+
+//adding event handler on specific (Other) select value
+
+$('#title').change(function() {
+    let show = $(this).val() == 'other';
+    $('#other-title').toggle(show);
+});
+
+//hide Select theme option from select
+
+$("#design option:first").attr('selected', 'selected').hide();
+
+//”T-Shirt Info” section STARTS
+
+//adding option Please select a T-shirt theme to select 
+function Tshirt() {
+
+    let $cornflowerblue = $('#color option[value="cornflowerblue"]');
+    let $darkslategrey = $('#color option[value="darkslategrey"]');
+    let $gold = $('#color option[value="gold"]');
+    let $tomato = $('#color option[value="tomato"]');
+    let $steelblue = $('#color option[value="steelblue"]');
+    let $dimgrey = $('#color option[value="dimgrey"]');
+
+    $cornflowerblue.hide();
+    $darkslategrey.hide();
+    $gold.hide();
+    $tomato.hide();
+    $steelblue.hide();
+    $dimgrey.hide();
+}
+//calling function 
+Tshirt();
+
+//setting listener on Design select element
+$('#design').change(function() {
+
+    //dynamically remove all elements from select and add elements that are matching
+    if ($(this).val() == 'js puns') {
+        //removing
+        $('#color option[value!=0]').remove()
+            //addin options
+        $('#color').append(new Option('Cornflower Blue (JS Puns shirt only)', 'cornflowerblue'));
+        $('#color').append(new Option('Dark Slate Grey (JS Puns shirt only)', 'darkslategrey'));
+        $('#color').append(new Option('Gold (JS Puns shirt only)', 'gold'));
+
+    } else if ($(this).val() == 'heart js') {
+        //removing
+        $('#color option[value!=0]').remove();
+        //adding options
+        $('#color').append(new Option('Tomato (I &#9829; JS shirt only)', 'tomato'));
+        $('#color').append(new Option('Steel Blue (I &#9829; JS shirt only)', 'steelblue'));
+        $('#color').append(new Option('Dim Grey (I &#9829; JS shirt only)', 'steelblue'));
+    }
+});
+//---------------------------------------------------------------------
+
+//”Register for Activities” section STARTS
+
+//creating total cost element and appending to DOM
+
+let $totalCostLabel = $('<label>Total cost</label>');
+$('.activities').append($totalCostLabel);
+
+let $totalActivityCost = 0;
+
+//setting listener for checkboxes
+
+$('.activities').change(function() {
+
+    if ($('input:checkbox[data-cost]')) {
+
+        let val = $(this);
+        console.log(val);
+    }
+
+});
+
+//-------------------------------------------------------------------
